1720636d44a0d9ceabaa920e458658425e854958
mhauer <mhauer@users.sourceforge.net>
2009-05-28 21:04:58 +0000

    Next version of the autotest script.  "Works better".  Less verbose on the log file and properly detects success and failure from GLD.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@234 54b63ff4-7b51-0410-8bda-9fa96797e505

--- validate.py
+++ validate.py
@@ -1,172 +1,117 @@
-import os, sys, time, shutil
-import email, smtplib
+import sys
+import email
+import os
+import shutil
+import smtplib
+import time
+import subprocess
 
-os.putenv("PATH","%PATH%;..\\..\\..\\VS2005\\Win32\\Release")
+##
+#	run_tests is the main function for the autotest validation script.
+#	@param	argv	The command line arguements.
+def run_tests(argv):
+	print("Starting autotest script")
 	
-def email_error():
-	return 0
-	#end email_error()
-
-def parse_outfile(filename):
-	return 0
-	# end parse_outfile
-
-# configuration directory
-# glm file name
-def build_conf(confdir, glmname):
-	#	fopen conf file
-	conf = open("gridlabd.conf", "w")
-	conf.write("// $Id$\n")
-	conf.write("// Copyright (C) 2009 Battelle Memorial Institute\n//\n")
-	conf.write("// Auto-generated gridlabd.conf for "+glmname+" by the autotest script\n")
-	conf.write("\n")
+	there_dir = os.getcwd()
+	err_ct = 0
 	
+	# determine where the script starts
+	here_dir = os.getcwd()
+	print("Starting from \'"+here_dir+"\'")
 	
-	#	set streams
-	#set include="../../core"
-	#include "../../core/rt/msvc_debugger.glm"
-	#include "assert.glm"
-	
-	#	set dump file
-	#	set debug=1
-	#	set warning=1
-	#	close conf file
-	return 0
-	# end build_conf
-
-
-#	basedir ~ for the glmfile
-#	testdir ~ for the 
-def run_file(basedir, testdir, glmname):
-	#	validate basedir
-	#	validate testdir
-	#	validate glmname
-	#	point to gridlabd.conf in testdir
-	#	set dump file to glmname + "_dump.glm"
-	#	set stdout to glmname + "_stdout.txt"
-	#	set stderr to glmname + "_stderr.txt"
-
-	# 	mkdir(glmname)
-	#	copy glmname.glm to testdir/glmname.glm
-	#	cd testdir/glmname
-	#	build conf file
-
-	#	set output to glmname + "_out.glm"
-	outpath = basedir+glmname+".xml"
-	dumppath = basedir+glmname+"_dump.glm"
-	#	rv = exec("gridlabd "+
-	return os.system("gridlabd --verbose -o "+outpath+" "+filepath)
-	#end run_file
-
-def print_header(logfile, logtime):
-	logfile.write("Log file opened at "+time.asctime(logtime)+"\n")
-	logfile.write("Working directory is \'"+os.getcwd()+"\'\n")
-	#end print_header
-
-def gld_validate(argv):
-	#	capture cwd
-	autotest_cwd = os.getcwd()
-	
-	#	if args = null, copy cwd to active dir
-	test_wd = os.getcwd()
+	#determine where we _want_ the script to start
 	if len(argv) > 1:
 		argn = 1
-		# scan for flags, else use as working dir
 		while argn < len(argv):
 			if argv[argn][0] == '-':
 				argn+=1
-			else :
-				# copy argv[argn] to autotest_cwd
-				test_wd = argv[argn]
+				# ignore anything that looks like a flag
+			else:
+				there_dir = argv[argn]
+				# take the first path and be done with it
+				# poor form but we're not doing much complicated
 				break
 	
-	# stuff?
-	
-	#	open autotest log file
-	logtime = time.localtime()
-	logtimestr = time.strftime("%Y%m%d_%H%M%S", logtime)
-	logfilename = "autotest_"+logtimestr+".log"
-	logfile = open(logfilename, 'w')
-	print_header(logfile, logtime)
-	
-	
-	#	walk active dir and cache autotest directories
-	autotest_list = []
-	for path, dirs, files in os.walk(test_wd): # os.path.walk before 3.0
+	#locate autotest dirs
+	#autotestdirs = find_autotest_dirs(there_dir)
+	autotestdirs = []
+	for path, dirs, files in os.walk(there_dir):
 		if "autotest" in dirs:
-			autotest_list.append(os.path.abspath(os.path.join(path, "autotest")))
+			autotestdirs.append(os.path.abspath(os.path.join(path,"autotest")))
 	
-	#	print autotest dir list to output file
-	print(autotest_list)
-	for path in autotest_list:
-		logfile.write("Found autotest directory \'"+path+"\'\n")
+	for path in autotestdirs:
+		print("Found dir: \'"+path+"\'")
 	
-	#	for each path in autotest list
-	result = 0
-	errcount = 0
-	errlist = []
-	for path in autotest_list: #	get file list for dir
-		print("path: \'"+path+"\'")
-		os.chdir(path)
-		print("chdir: \'"+os.getcwd()+"\'")
-		#	for each file
-		print(os.listdir('.'))
-		for file in os.listdir('.'):
-			#	if it ends with ".glm"'
+	#locate autotest test files
+	#autotestfiles = find_autotest_files(autotestdirs)
+	autotestfiles = []
+	for path in autotestdirs:
+		for file in os.listdir(path):
 			if "test_" in file and ".glm" in file:
-				#	base dir is where the GLM file resides
-				base = file[:-4]
-				if not os.path.exists(base):
-					os.mkdir(base)
-				
-				shutil.copy2(file, base)
-				os.chdir(base)
-				
-				#	rv = run_file(base dir, autotest dir, glm file name - ".glm")
-				if not os.path.exists(file):
-					logfile.write("ERROR:  Error copying "+file+" to "+path+".\n")
-					result = -1
+				autotestfiles.append((path, file))
+	
+	for path, file in autotestfiles:
+		print("Found file: \'"+file+"\'")
+	
+	#build test dirs
+	#for file in autotestfiles:
+	errlist=[]
+	errct = 0
+	for path, file in autotestfiles:
+		err = False
+		slice = file[:-4]
+		xpath = os.path.join(path,slice) # path/slice
+		xfile = os.path.join(xpath,file) # path/slice/file.glm
+		if not os.path.exists(xpath):
+			os.mkdir(xpath)
+		shutil.copy2(os.path.join(path,file), xfile) # path/file to path/slice/file
+		
+		# build conf files
+		# moo?
+		
+		#run files with popen
+		#run file with:
+		outfile = open(os.path.join(xpath,"outfile.txt"), "w")
+		errfile = open(os.path.join(xpath,"errfile.txt"), "w")
+		rv = subprocess.call(["gridlabd",xfile],stdout=outfile,stderr=errfile)
+		outfile.close()
+		errfile.close()
+		
+		# handle results
+		if "err_" in file:
+			if rv == 0:
+				if "opt_" in file:
+					print("WARNING: Optional file "+file+" converged when it shouldn't've!")
+					err = False
 				else:
-					#build conf file
-					#	cwd usage ~ we should be in the directory the current file is in ~ MH
-					#build_conf(os.getcwd(), file) # in development
-					#run GLD
-					logfile.write("RUN: \'gridlabd "+os.path.abspath(file)+"\'\n")
-					result = os.system("gridlabd --verbose --debug \""+os.path.abspath(file)+"\"")
-				
-				os.chdir('..')
-				print("chdir: \'"+os.getcwd()+"\'")
-				
-				#	if rv is not empty
-				if result > 0:
-					#	if strstr(file name, "_opt")
-					if "opt_" in file:
-						#	fprintf warning
-						logfile.write("WARNING:  Optional test \'"+file+"\' failed to converge.\n")
-					#	else
-					elif not "err_" in file:
-						#	fprintf error
-						logfile.write("ERROR:  Test \'"+file+"\' failed to converge.\n")
-						#	add to error list
-						errlist.append(os.path.join(path, base))
-						#	++error
-						errcount+=1
+					print("ERROR: "+file+" converged when it shouldn't've!")
+					errct += 1
+					err = True
 			else:
-				logfile.write("SUCCESS:  Test \'"+file+"\' passed.\n")
-				
-	#	if error > 0
-	if errcount > 0:
-		# where are the email settings fetched from?
-		#	email_error(error_ct, error_list)
-		logfile.write(str(errcount)+" errors were found.\n")
-	else:
-		logfile.write("No errors were found.\n")
-	logfile.close()
-	return errcount
-	#end gld_validate()
-
+				print("SUCCESS: File "+file+" failed to converge, as planned.")
+		else:
+			if rv != 0:
+				if "opt_" in file:
+					print("WARNING: Optional file "+file+" failed to converge!")
+					err = False
+				else:
+					errct += 1
+					print("ERROR: "+file+" failed to converge!")
+					err = True
+			else:
+				print("SUCCESS: File "+file+" converged successfully.")
+		if err:
+			# zip target directory
+			errlist.append((path,file))
+		# end autotestfiles loop
+	
+	#return success/failure
+	print("Validation detected "+str(errct)+" models with errors.")
+	return errct
+#end run_tests()
 
 if __name__ == '__main__':
-	gld_validate(sys.argv)
-	#end main
+	run_tests(sys.argv)
+#end main
 
+#end validate_new.py
