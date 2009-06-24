5f1ae5b8479d0d8e321699e5f91d19495d67ec0c
Frank Tuffner <Francis.Tufner@pnnl.gov>
2009-06-24 14:26:31 +0000

    Slight change to the superLU installation batch file to ensure nothing odd gets returned if superLU is already extracted.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@334 54b63ff4-7b51-0410-8bda-9fa96797e505

--- VS2005/install_superLU.bat
+++ VS2005/install_superLU.bat
@@ -1,6 +1,10 @@
 cd ..\third_party
 
+IF EXIST .\SuperLU_3.1\SRC GOTO END
+
 copy superlu_3.1.tar.gz superlu_3.1_copy.tar.gz
 bin\gzip.exe -d superlu_3.1_copy.tar.gz
 bin\tar -xvf superlu_3.1_copy.tar
 del superlu_3.1_copy.tar
+
+:END
