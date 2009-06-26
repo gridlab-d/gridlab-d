c5e3fb6ce4a6771c47508fdbc1ddc10790864397
juanisu <juanisu@users.sourceforge.net>
2009-06-26 02:58:08 +0000

    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@343 54b63ff4-7b51-0410-8bda-9fa96797e505

--- powerflow/solver_nr.h
+++ powerflow/solver_nr.h
@@ -18,7 +18,11 @@ typedef struct  {
 	double PG[3];///< real power generation at generator bus
 	double QG[3];///< reactive power generation at generator bus
 	double kv_base; ///< kV basis
-	double mva_base; /// MVA basis
+    double mva_base; /// MVA basis
+	double Jacob_A[3]; // Element a in equation (37), which is used to update the Jacobian matrix at each iteration
+	double Jacob_B[3]; // Element b in equation (38), which is used to update the Jacobian matrix at each iteration
+	double Jacob_C[3]; // Element c in equation (39), which is used to update the Jacobian matrix at each iteration
+	double Jacob_D[3]; // Element d in equation (40), which is used to update the Jacobian matrix at each iteration
 } BUSDATA;
 
 typedef struct {
@@ -37,7 +41,7 @@ typedef struct {
 typedef struct {
 	int row_ind;  ///< row location of the element in n*n bus admittance matrix in NR solver
 	int	col_ind;  ///< collumn location of the element in n*n bus admittance matrix in NR solver
-    complex Y[3][3]; ///< 3*3 complex value of the element in bus admittance matrix in NR solver
+    complex Y[3][3]; ///< complex value of elements in bus admittance matrix in NR solver
 } Bus_admit;
 
 
