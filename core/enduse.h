a59d678c6cb4cac50745e58010c5a0b09ddaf7d6
nirupamapk <nirupamapk@users.sourceforge.net>
2009-07-24 18:32:43 +0000

    Change for enduse_publish
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@536 54b63ff4-7b51-0410-8bda-9fa96797e505

--- core/enduse.h
+++ core/enduse.h
@@ -2,12 +2,18 @@
 #ifndef _ENDUSE_H
 #define _ENDUSE_H
 
+#define NULL    0
+
 #include "class.h"
 #include "timestamp.h"
 #include "loadshape.h"
 
+#define EUF_IS220 0x0001 ///< enduse flag to indicate that the voltage is line-to-line, not line-to-neutral
+
 typedef struct s_enduse {
+	char *name;
 	loadshape *shape;
+	long flags;
 	complex power;				/* power in kW */
 	complex energy;				/* total energy in kWh */
 	complex demand;				/* maximum power in kW (can be reset) */
@@ -29,6 +35,7 @@ TIMESTAMP enduse_sync(enduse *e, PASSCONFIG pass, TIMESTAMP t0, TIMESTAMP t1);
 TIMESTAMP enduse_syncall(TIMESTAMP t1);
 int convert_to_enduse(char *string, void *data, PROPERTY *prop);
 int convert_from_enduse(char *string,int size,void *data, PROPERTY *prop);
+int enduse_publish(CLASS *oclass, int struct_address, char *prefix);
 int enduse_test(void);
 
 #endif
