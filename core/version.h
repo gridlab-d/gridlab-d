4cd9aa8542d85f58d088f7a3469f7e75624dff3c
Dave Chassin <dchassin@slac.stanford.edu>
2010-01-18 20:17:32 +0000

    Change build version to 2.1 Eldorado (never too late to do the right thing).
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@1255 54b63ff4-7b51-0410-8bda-9fa96797e505

--- core/version.h
+++ core/version.h
@@ -1,7 +1,7 @@
 /* automatically created by ./checkpkgs */
 #define REV_MAJOR 2
-#define REV_MINOR 0
+#define REV_MINOR 1
 #define REV_PATCH 0
 #define COPYRIGHT "Copyright (C) 2004-2009\nBattelle Memorial Institute\nAll Rights Reserved"
 #define BUILD "$Revision: 0 $"
-#define BRANCH "Diablo" 
+#define BRANCH "Eldorado" 
