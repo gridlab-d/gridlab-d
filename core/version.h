042fab312f213d989193ecc6ce284a346c90a93a
mhauer <mhauer@users.sourceforge.net>
2010-01-27 16:30:11 +0000

    Added re-include guards. Added CRT_SECURE_NO_WARNINGS to superLU and superLU_MT projects to suppress the hundreds of warning messages from using sprintf and the like instead of Microsoft's "secure" variants.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@1282 54b63ff4-7b51-0410-8bda-9fa96797e505

--- core/version.h
+++ core/version.h
@@ -1,7 +1,15 @@
 /* automatically created by ./checkpkgs */
+
+#ifndef _GRIDLABD_VERSION_H
+#define _GRIDLABD_VERSION_H
+
 #define REV_MAJOR 2
 #define REV_MINOR 1
 #define REV_PATCH 0
 #define COPYRIGHT "Copyright (C) 2004-2009\nBattelle Memorial Institute\nAll Rights Reserved"
 #define BUILD "$Revision: 0 $"
 #define BRANCH "Eldorado" 
+
+#endif
+
+// EOF
