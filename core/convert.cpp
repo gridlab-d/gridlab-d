9f55a3af0ced727abbe83408d5eac05b7d8bd9c5
Jason Fuller <Jason.Fuller@pnnl.gov>
2010-01-28 20:03:43 +0000

    Changed enum definitions back to 32-bit so that enumerations would work again.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@1289 54b63ff4-7b51-0410-8bda-9fa96797e505

--- core/convert.cpp
+++ core/convert.cpp
@@ -249,7 +249,7 @@ int convert_to_enumeration(char *buffer, /**< a pointer to the string buffer */
 	{
 		if (strcmp(keys->name,buffer)==0)
 		{
-			*(unsigned int64*)data=keys->value;
+			*(unsigned long*)data=keys->value;
 			found = true;
 			break;
 		}
