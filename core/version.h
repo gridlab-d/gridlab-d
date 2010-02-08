23471b498fa5cd506de4caab54fae3e424a01358
Dave Chassin <dchassin@slac.stanford.edu>
2010-02-08 19:40:58 +0000

    Fixed build number for non-Win32 builds.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@1322 54b63ff4-7b51-0410-8bda-9fa96797e505

--- core/version.h
+++ core/version.h
@@ -7,7 +7,11 @@
 #define REV_MINOR 1
 #define REV_PATCH 0
 #define COPYRIGHT "Copyright (C) 2004-2009\nBattelle Memorial Institute\nAll Rights Reserved"
+#ifdef WIN32
 #define BUILD "$Revision: 0 $"
+#else
+#include "build.h"
+#endif
 #define BRANCH "Eldorado" 
 
 #endif
