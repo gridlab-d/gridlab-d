b3e67273c1b1e08f6b302a03b1724de620774e05
Nathan Tenney <Nathan.Tenney@pnnl.gov>
2010-03-01 23:35:07 +0000

    Fixes to have the update_sourceforge task operate on the new release location.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@1365 54b63ff4-7b51-0410-8bda-9fa96797e505

--- VS2005/update_sourceforge.py
+++ VS2005/update_sourceforge.py
@@ -1,4 +1,4 @@
-import os,stat,time
+import os,stat,time,sys
 
 #######################################################################
 # Delete any files older than 30 days.
@@ -6,15 +6,22 @@ import os,stat,time
 #
 
 thirty_days = 60 * 60 * 24 * 30 # Thirty days in seconds
-SF_PATH = "L:\\"
+
 
 def testFile(fileName):
 	ct = int(time.time())
 	file_time = os.stat(fileName)[stat.ST_CTIME]
 	
+	#print file_time, ct-thirty_days,ct-thirty_days > file_time
 	return (ct - thirty_days) > file_time
 
 if __name__ == "__main__":
+	SF_PATH = "L:\\"
+	if(len(sys.argv) > 1):
+		SF_PATH=sys.argv[1]
+	
 	for filename in os.listdir(SF_PATH):
-		if testFile(SF_PATH + filename):
-			os.unlink(SF_PATH + filename)
+		#print "Testing",SF_PATH + "\\" +filename
+		if testFile(SF_PATH + "\\" +filename):
+			print "delete",SF_PATH + "\\" +filename
+			os.unlink(SF_PATH + "\\" +filename)
