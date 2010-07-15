6d524068746e3aa1d25c8471a8ff25017d3a4659
Nathan Tenney <Nathan.Tenney@pnnl.gov>
2010-07-15 23:14:55 +0000

    Modified update sourceforge to make it more flexible.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@1675 54b63ff4-7b51-0410-8bda-9fa96797e505

--- VS2005/update_sourceforge.py
+++ VS2005/update_sourceforge.py
@@ -1,27 +1,37 @@
-import os,stat,time,sys
+import os,stat,time,sys,re
+from datetime import datetime
 
 #######################################################################
 # Delete any files older than 30 days.
 #
 #
+day_secs = 60 * 60 * 24 # one day in seconds
 
-thirty_days = 60 * 60 * 24 * 30 # Thirty days in seconds
 
 
-def testFile(fileName):
-	ct = int(time.time())
-	file_time = os.stat(fileName)[stat.ST_CTIME]
+def testFile(fileName,days):
+	pattern = re.compile("gridlabd-win32-([0-9]{4})_([0-9]{2})_([0-9]{2})-nightly\\.exe")
+	result = pattern.match(fileName)
 	
-	#print file_time, ct-thirty_days,ct-thirty_days > file_time
-	return (ct - thirty_days) > file_time
+	d = datetime(int(result.group(1)),int(result.group(2)),int(result.group(3)))
+	td = datetime.today() - d
+	
+	return td.days >= days
 
 if __name__ == "__main__":
 	SF_PATH = "L:\\"
+	days=30
+	DRY_RUN = False
 	if(len(sys.argv) > 1):
 		SF_PATH=sys.argv[1]
+	if(len(sys.argv) > 2):
+		days = int(sys.argv[2])
+	if(len(sys.argv) > 3):
+		DRY_RUN = True
 	
 	for filename in os.listdir(SF_PATH):
 		#print "Testing",SF_PATH + "\\" +filename
-		if testFile(SF_PATH + "\\" +filename):
+		if testFile(filename,days):
 			print "delete",SF_PATH + "\\" +filename
-			os.unlink(SF_PATH + "\\" +filename)
+			if(not DRY_RUN):
+				os.unlink(SF_PATH + "\\" +filename)
