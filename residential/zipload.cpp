703aebf66b596550d51bf67253a94a2112859d02
Jason Fuller <Jason.Fuller@pnnl.gov>
2010-07-22 01:29:20 +0000

    Initial implementation of population dynamics.  Still needs further testing and development.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@1710 54b63ff4-7b51-0410-8bda-9fa96797e505

--- residential/zipload.cpp
+++ residential/zipload.cpp
@@ -8,6 +8,11 @@
 	Individual power fractions and power factors are used.
 	Heat fraction ratio is used to calculate the internal gain from ZIPload loads.
 
+	The demand response model is based from a paper written by David Chassin and Jason Fuller
+	"On the Equilibrium Dynamics of Demand Response in Thermostatic Loads" accepted by HICSS
+	 - This model is very much in the preliminary stage and only looks at the equilibrium solutions
+	   of a population of devices as a function of customer demand and thermostatic duty cycles
+
  @{
  **/
 
@@ -15,6 +20,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include <math.h>
+#include <float.h>
+#include <memory.h>
 
 #include "house_a.h"
 #include "zipload.h"
@@ -45,6 +52,26 @@ ZIPload::ZIPload(MODULE *module) : residential_enduse(module)
 			PT_double,"impedance_pf",PADDR(impedance_pf), PT_DESCRIPTION, "power factor for constant impedance portion",
 			PT_bool,"is_240",PADDR(is_240), PT_DESCRIPTION, "load is 220/240 V (across both phases)",
 			PT_double,"breaker_val[A]",PADDR(breaker_val), PT_DESCRIPTION, "Amperage of connected breaker",
+
+			// Variables for demand response mode
+			PT_bool,"demand_response_mode",PADDR(demand_response_mode), PT_DESCRIPTION, "Activates equilibrium dynamic representation of demand response",
+			PT_int16,"number_of_devices",PADDR(N), PT_DESCRIPTION, "Number of devices to model - base power is the total load of all devices",
+			PT_int16,"thermostatic_control_range",PADDR(L),PT_DESCRIPTION, "Range of the thermostat's control operation",
+			PT_double,"number_of_devices_off",PADDR(N_off),PT_DESCRIPTION, "Total number of devices that are off",
+			PT_double,"number_of_devices_on",PADDR(N_on),PT_DESCRIPTION, "Total number of devices that are on",
+			//PT_double,"density_of_devices_off[1/K]",PADDR(noff),PT_DESCRIPTION, "Density of devices that are off per unit of temperature",
+			//PT_double,"density_of_devices_on[1/K]",PADDR(non),PT_DESCRIPTION, "Density of devices that are on per unit of temperature",
+			PT_double,"rate_of_cooling[K/h]",PADDR(roff),PT_DESCRIPTION, "rate at which devices cool down",
+			PT_double,"rate_of_heating[K/h]",PADDR(ron),PT_DESCRIPTION, "rate at which devices heat up",
+			//PT_double,"total_cycle_time[h]",PADDR(t), PT_DESCRIPTION, "total cycle time of a thermostatic device",
+			//PT_double,"total_off_time[h]",PADDR(toff),PT_DESCRIPTION, "total off time of device",
+			//PT_double,"total_on_time[h]",PADDR(ton),PT_DESCRIPTION, "total on time of device",
+			PT_int16,"temperature",PADDR(x),PT_DESCRIPTION, "temperature of the device's controlled media (eg air temp or water temp)",
+			PT_double,"duty_cycle",PADDR(phi),PT_DESCRIPTION, "duty cycle of the device",
+			//PT_double,"diversity_of_population",PADDR(PHI),PT_DESCRIPTION, "diversity of a population of devices",
+			PT_double,"demand_rate[1/h]",PADDR(eta),PT_DESCRIPTION, "consumer demand rate that prematurely turns on a device or population",
+			//PT_double,"effective_rate[K/h]",PADDR(rho),PT_DESCRIPTION, "effect rate at which devices heats up or cools down under consumer demand",
+			PT_double,"nominal_power",PADDR(nominal_power),PT_DESCRIPTION, "the rated amount of power demanded by devices that are on",
 			NULL)<1) 
 
 			GL_THROW("unable to publish properties in %s",__FILE__);
@@ -70,6 +97,11 @@ int ZIPload::create()
 	load.config = 0x0;	//110 by default
 	breaker_val = 1000.0;	//Obscene value so it never trips
 
+	demand_response_mode = false;
+	ron = roff = -1;
+	phi = eta = 0.2;	
+	next_time = 0;
+
 	//Default values of other properties
 	power_pf = current_pf = impedance_pf = 1.0;
 
@@ -81,6 +113,82 @@ int ZIPload::init(OBJECT *parent)
 {
 	OBJECT *hdr = OBJECTHDR(this);
 	hdr->flags |= OF_SKIPSAFE;
+
+	if (demand_response_mode == true)
+	{
+		gl_warning("zipload_init: The demand response zipload is an experimental model at this time.");
+		
+		// Initial error checks
+		if (abs(eta) >= 1)
+			GL_THROW("zipload_init: demand_rate (eta) must be between (and not include) -1 and 1.");
+		if (phi <= 0 || phi >= 1)
+			GL_THROW("zipload_init: duty_cycle (phi) must be between (and not include) 0 and 1.");
+		
+		
+		// Set up the buffers and perform some error checks
+		if (L > 0)
+			drm.nbins = L;
+		else
+			GL_THROW("zipload_init: thermostatic_control_range (L) must be greater than 0.");
+
+		drm.off = (double*)malloc(sizeof(double)*L);
+		drm.on = (double*)malloc(sizeof(double)*L);
+		if (drm.off==NULL || drm.on==NULL)
+			GL_THROW("zipload_init: memory allocation error.  Please report this error.");
+
+		/* setup the initial population */
+		if (ron == -1 || roff == -1)
+		{
+			if (phi <= 0.5)
+			{
+				roff = phi/(1-phi);
+				ron = 1;
+				gl_warning("ron or roff was not set.  Using phi to calculate.");
+			}
+			else
+			{
+				roff = 1;
+				ron = (1-phi)/phi;
+				gl_warning("ron or roff was not set.  Using phi to calculate.");
+			}
+		}
+		else
+			phi = roff / (ron + roff);
+
+		if (roff < 0 || ron < 0)
+			GL_THROW("zipload_init: rate_of_cooling and rate_of_heating must be greater than or equal to 0.");
+
+		non = noff = 0;
+		double test_N = 0;
+
+		for (x=0; x<L; x++)
+		{
+			/* exponential distribution */
+			if (eta != 0)
+			{
+				drm.on[x] = N * eta * (1-phi) * exp(eta*(L-0.5-x)/roff) / (exp(eta*L/roff)-1);
+				drm.off[x] = drm.on[x] * ron/roff;
+				test_N += drm.on[x] + drm.off[x];
+				//non += drm.on[x] = eta * (1-phi) * exp(eta*(L-x+0.5)/roff) / (exp(eta*L/roff)-1);
+				//noff += drm.off[x] = drm.on[x]*ron/roff;
+			}
+
+			/* uniform distribution */
+			else
+			{
+				non += drm.on[x] = N * phi/L;
+				noff += drm.off[x] = N * (1-phi)/L;
+			}
+		}
+
+		/* check for valid population */
+		if (abs(test_N - N) != 0)
+		{
+			double extra = N - test_N;
+			drm.off[0] += extra;
+		}
+		
+	}
 
 	if (is_240)	//See if the 220/240 flag needs to be set
 	{
@@ -104,6 +212,62 @@ TIMESTAMP ZIPload::sync(TIMESTAMP t0, TIMESTAMP t1)
 	double imag_power = 0.0;
 	double angleval;
 
+	if (demand_response_mode == true && next_time <= t1)
+	{
+		double dNon,dNoff;		
+		double hold_off;	
+
+		N_on = N_off = 0;
+
+		for (int jj=0; jj<L; jj++)
+		{
+			//previous_drm.on[jj] = drm.on[jj];
+			//previous_drm.off[jj] = drm.off[jj];
+
+			if (eta > 0)
+			{			
+				if (jj != (L-1))
+					dNon = -ron * drm.on[jj] + eta * drm.off[jj] + ron * drm.on[jj+1];
+				else
+					dNon = -ron * drm.on[jj] + (1 - eta) * drm.off[jj] * roff + eta * drm.off[jj];
+				
+				if (jj != 0)
+					dNoff = -(1 - eta) * drm.off[jj] * roff - eta * drm.off[jj] + (1 - eta) * hold_off * roff;
+				else
+					dNoff = -(1 - eta) * drm.off[jj] * roff - eta * drm.off[jj] + ron * drm.on[jj];
+			}
+			else
+			{
+				if (jj != (L-1))
+					dNon = -ron * (1 + eta) * drm.on[jj] + eta * drm.on[jj] + ron * (1 + eta) * drm.on[jj+1];
+				else
+					dNon = -ron * (1 + eta) * drm.on[jj] + eta * drm.on[jj] + roff * drm.off[jj];
+				
+				if (jj != 0)
+					dNoff = -roff * drm.off[jj] - eta * drm.on[jj] + hold_off * roff;
+				else
+					dNoff = -roff * drm.off[jj] - eta * drm.on[jj] + ron * (1 + eta) * drm.on[jj];
+			}
+
+			hold_off = drm.off[jj];
+
+			drm.on[jj] = drm.on[jj] + dNon;
+			N_on += drm.on[jj];
+			drm.off[jj] = drm.off[jj] + dNoff;
+		}
+
+		N_off = N - N_on;
+
+		nominal_power = base_power * N_on / N;
+		double R = ron;
+		int dt = 0;
+		if (ron < roff)
+			R = roff;
+		dt = int(1/R * 3600);
+
+		next_time = t1 + dt;
+	}
+
 	if (pCircuit!=NULL){
 		if (is_240)
 		{
@@ -120,9 +284,13 @@ TIMESTAMP ZIPload::sync(TIMESTAMP t0, TIMESTAMP t1)
 	if (pCircuit->status==BRK_CLOSED) 
 	{
 		//All values placed as kW/kVAr values - to be consistent with other loads
+		double demand_power = base_power;
+		
+		if (demand_response_mode == true)
+			demand_power = nominal_power;
 
 		//Calculate power portion
-		real_power = base_power * load.power_fraction;
+		real_power = demand_power * load.power_fraction;
 
 		imag_power = (power_pf == 0.0) ? 0.0 : real_power * sqrt(1.0/(power_pf * power_pf) - 1.0);
 
@@ -134,7 +302,7 @@ TIMESTAMP ZIPload::sync(TIMESTAMP t0, TIMESTAMP t1)
 		load.power.SetRect(real_power,imag_power);
 
 		//Calculate current portion
-		real_power = base_power * load.current_fraction;
+		real_power = demand_power * load.current_fraction;
 
 		imag_power = (current_pf == 0.0) ? 0.0 : real_power * sqrt(1.0/(current_pf * current_pf) - 1.0);
 
@@ -146,7 +314,7 @@ TIMESTAMP ZIPload::sync(TIMESTAMP t0, TIMESTAMP t1)
 		load.current.SetRect(real_power,imag_power);
 
 		//Calculate impedance portion
-		real_power = base_power * load.impedance_fraction;
+		real_power = demand_power * load.impedance_fraction;
 
 		imag_power = (impedance_pf == 0.0) ? 0.0 : real_power * sqrt(1.0/(impedance_pf * impedance_pf) - 1.0);
 
@@ -177,6 +345,8 @@ TIMESTAMP ZIPload::sync(TIMESTAMP t0, TIMESTAMP t1)
 		load.power_factor = 0.0;
 	}
 
+	if (next_time < t2)
+		t2 = next_time;
 	return t2;
 }
 
--- residential/zipload.h
+++ residential/zipload.h
@@ -22,6 +22,35 @@ public:
 	bool is_240;				///< load connected at 220 
 	double breaker_val;			///< Amperage limit for connected breaker
 
+	bool demand_response_mode;	///< Activates equilibrium dynamic representation of demand response 
+	int64 N;					///< Number of devices to model - base power is per device 
+	int16 L;					///< Range of the thermostat's control operation 
+	double N_off;				///< Number of devices that are off 
+	double N_on;					///< Number of devices that are on 
+	double noff;				///< Density of devices that are off per unit of temperature 
+	double non;					///< Density of devices that are on per unit of temperature 
+	double roff;				///< rate at which devices cool down 
+	double ron;					///< rate at which devices heat up 
+	double t; 					///< total cycle time of a thermostatic device 
+	double toff;				///< total off time of device 
+	double ton;					///< total on time of device 
+	int16 x;					///< temperature of the device's controlled media (eg air temp or water temp) 
+	double phi;					///< duty cycle of the device 
+	double PHI;					///< diversity of a population of devices 
+	double eta;					///< consumer demand rate that prematurely turns on a device or population 
+	double rho;					///< effect rate at which devices heats up or cools down under consumer demand 
+	double nominal_power;
+	int64 next_time;
+
+	typedef struct {
+		double *on;
+		double *off;
+		int16 nbins;
+	} DRMODEL;
+
+	DRMODEL drm;
+	DRMODEL previous_drm;			///< structures to save drm population and previous population
+
 public:
 	static CLASS *oclass, *pclass;
 	static ZIPload *defaults;
