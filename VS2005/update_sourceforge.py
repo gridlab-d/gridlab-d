7571e21de9ce3da642aad44319759d736c107e43
Nathan Tenney <Nathan.Tenney@pnnl.gov>
2010-08-04 00:28:05 +0000

    Changed command line handling for update_sourceforge.py
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@1775 54b63ff4-7b51-0410-8bda-9fa96797e505

--- VS2005/update_sourceforge.py
+++ VS2005/update_sourceforge.py
@@ -1,5 +1,6 @@
-import os,stat,time,sys,re
+import os,stat,time,sys,re,getopt
 from datetime import datetime
+
 
 #######################################################################
 # Delete any files older than 30 days.
@@ -7,31 +8,50 @@ from datetime import datetime
 #
 day_secs = 60 * 60 * 24 # one day in seconds
 
+def do_help():
+	print("update_sourceforge.py - GridLAB-D autotest/validation script")
+	print("     update_sourceforge.py [--rdir=<dir>] [--days=<days>] [--prefix=<prefix>]")
+	return 0
 
-
-def testFile(fileName,days):
-	pattern = re.compile("gridlabd-win32-([0-9]{4})_([0-9]{2})_([0-9]{2})-nightly\\.exe")
+def testFile(pattern,fileName,days,prefix):
 	result = pattern.match(fileName)
-	
+	if result == None:
+		print "No files match pattern", pattern.pattern
+		return 
+		
 	d = datetime(int(result.group(1)),int(result.group(2)),int(result.group(3)))
 	td = datetime.today() - d
 	
 	return td.days >= days
 
 if __name__ == "__main__":
-	SF_PATH = "L:\\"
-	days=30
+	SF_PATH = "../../releases"
+	days=5
 	DRY_RUN = False
-	if(len(sys.argv) > 1):
-		SF_PATH=sys.argv[1]
-	if(len(sys.argv) > 2):
-		days = int(sys.argv[2])
-	if(len(sys.argv) > 3):
-		DRY_RUN = True
+	prefix = "gridlabd-win32-"
+	
+	opts, args = getopt.getopt(sys.argv[1:], "",["rdir=",'days=','prefix=','dry-run','help'])
+    	
+	for o,a in opts:
+		if "--help" in o or "-h" in o:
+			do_help();
+			sys.exit(0)
+		elif "--rdir" in o:
+			SF_PATH = a
+		elif "--days" in o:
+			days = int(a)
+		elif "--prefix" in o:
+			prefix = a
+		elif "--dry-run" in o:
+			DRY_RUN = True
+		else:
+			print "Invalid argument"
+			do_help()
+	pattern = re.compile(prefix+"([0-9]{4})_([0-9]{2})_([0-9]{2})-nightly\\.exe")
 	
 	for filename in os.listdir(SF_PATH):
 		#print "Testing",SF_PATH + "\\" +filename
-		if testFile(filename,days):
+		if testFile(pattern,filename,days,prefix):
 			print "delete",SF_PATH + "/" +filename
 			if(not DRY_RUN):
 				os.unlink(SF_PATH + "/" +filename)
