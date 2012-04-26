f48ff605b95fc3739dfc7752e13503b70c33997d
Dave Chassin <dchassin@slac.stanford.edu>
2012-04-26 03:48:49 +0000

    Added the new callbacks for rt/gridlabd.h compare ops.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@3130 54b63ff4-7b51-0410-8bda-9fa96797e505

--- core/rt/gridlabd.h
+++ core/rt/gridlabd.h
@@ -867,6 +867,8 @@ typedef struct s_callbacks {
 		char *(*get_unit)(OBJECT *, char *);
 		void *(*get_addr)(OBJECT *, char *);
 		int (*set_value_by_type)(PROPERTYTYPE,void *data,char *);
+		bool (*compare_basic)(PROPERTYTYPE ptype, PROPERTYCOMPAREOP op, void* x, void* a, void* b, char *part);
+		PROPERTYCOMPAREOP (*get_compare_op)(PROPERTYTYPE ptype, char *opstr);
 	} properties;
 	struct {
 		struct s_findlist *(*objects)(struct s_findlist *,...);
