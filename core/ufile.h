8e3a0516e3a9ffb89d2e5b6473a5d3722077c5e7
Dave Chassin <dchassin@slac.stanford.edu>
2012-10-19 03:40:46 +0000

    Stub of ufile added.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@3537 54b63ff4-7b51-0410-8bda-9fa96797e505

---
+++ core/ufile.h
@@ -0,0 +1,16 @@
+/* $Id$
+ * Copyright (C) 2012 Battelle Memorial Institute
+ */
+
+#ifndef _UFILE_H
+#define _UFILE_H
+
+typedef struct s_ufile {
+	enum {UFT_NONE, UFT_FILE, UFT_HTTP} type;
+	void *handle;
+} UFILE;
+
+UFILE *uopen(char *fname, void *arg);
+size_t uread(void *buffer, size_t count, UFILE *rp);
+
+#endif
