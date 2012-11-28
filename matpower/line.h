0ae364148d5ceda73180f1a8b4b43b97c22971c3
anderk57 <anderk57@users.sourceforge.net>
2012-11-28 21:59:34 +0000

    matpower
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@3637 54b63ff4-7b51-0410-8bda-9fa96797e505

---
+++ matpower/line.h
@@ -0,0 +1,58 @@
+/** $Id: line.h 683 2008-06-18 20:16:29Z d3g637 $
+	@file line.h
+	@addtogroup line
+	@ingroup MODULENAME
+
+ @{
+ **/
+
+#ifndef _line_H
+#define _line_H
+
+//#include <iostream.h>
+#include <stdarg.h>
+#include "gridlabd.h"
+
+class line {
+private:
+	/* TODO: put private variables here */
+protected:
+	/* TODO: put unpublished but inherited variables */
+public:
+	/* TODO: put published variables here */
+	static CLASS *oclass;
+	static line *defaults;
+	
+	// Variables
+	int	F_BUS;
+	int	T_BUS;
+	double	BR_R;
+	double 	BR_X;
+	double 	BR_B;
+	double 	RATE_A;
+	double 	RATE_B;
+	double 	RATE_C;
+	double 	TAP;
+	double 	SHIFT;
+	int	BR_STATUS;
+	double	ANGMIN;
+	double 	ANGMAX;
+public:
+	/* required implementations */
+	line(MODULE *module);
+	int create(void);
+	int init(OBJECT *parent);
+	TIMESTAMP presync(TIMESTAMP t0, TIMESTAMP t1);
+	TIMESTAMP sync(TIMESTAMP t0, TIMESTAMP t1);
+	TIMESTAMP postsync(TIMESTAMP t0, TIMESTAMP t1);
+
+
+#ifdef OPTIONAL
+	static CLASS *pclass; /**< defines the parent class */
+	TIMESTAMP plc(TIMESTAMP t0, TIMESTAMP t1); /**< defines the default PLC code */
+#endif
+};
+
+#endif
+
+/**@}*/
---
+++ matpower/solver_matpower.h
@@ -0,0 +1,20 @@
+/** $Id: solver_matpower.h 683 2008-06-18 20:16:29Z d3g637 $
+	@file solver_matpower.h
+	@addtogroup solver_matpower
+	@ingroup MODULENAME
+**/
+
+#ifndef solverMatpower
+#define solverMatpower
+
+#include "libopf.h"
+#include "matrix.h"
+
+inline mxArray* initArray(double rdata[], int nRow, int nColumn);
+
+
+
+extern int solver_matpower();
+
+
+#endif
