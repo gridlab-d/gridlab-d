1fc7c521197a64534fd06bb700c5bfe95475bcf5
Dave Chassin <dchassin@slac.stanford.edu>
2012-12-19 20:57:01 +0000

    Added a check in buildnum.bat for whether svn.exe is installed
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@3713 54b63ff4-7b51-0410-8bda-9fa96797e505

--- utilities/buildnum.bat
+++ utilities/buildnum.bat
@@ -1,5 +1,9 @@
 @echo off
 
+set exe=svn.exe
+for %%a in ( "%PATH:;=" "%" ) do ( if exist "%%a\svn.exe" set exe="%%a\svn.exe" )
+if "%exe:"=%" == "svn.exe" goto noexe
+
 set dir=%1
 if '%dir%' == '' set dir=..
 set file=.\build.h
@@ -20,6 +24,11 @@ if %oldrev% == %newrev% goto done
 :update
 echo %file% updated
 echo #define BUILDNUM %newrev% >%file%
+goto done
+
+:noexe
+echo %exe% not found: try reinstalling Tortoise with the command line version enabled.
+goto done
 
 :done
 echo Current build is %newrev%
