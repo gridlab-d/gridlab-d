30b717d8c5372d8cfad405fd7af49302ee7a347c
Dave Chassin <dchassin@slac.stanford.edu>
2014-12-20 19:22:14 +0000

    (trunk) Undoing change to database.h for ticket 918 because it's not sufficient.
    git-svn-id: https://svn.code.sf.net/p/gridlab-d/code/trunk@4990 54b63ff4-7b51-0410-8bda-9fa96797e505

--- mysql/database.h
+++ mysql/database.h
@@ -42,6 +42,7 @@ EXTERN char default_schema[256] INIT("gridlabd");
 EXTERN int32 default_port INIT(3306);
 EXTERN char default_socketname[1024] INIT("/tmp/mysql.sock");
 EXTERN int64 default_clientflags INIT(CLIENT_LOCAL_FILES);
+EXTERN MYSQL *mysql_client INIT(NULL); ///< connection handle
 
 #define DBO_SHOWQUERY 0x0001 ///< show SQL query when verbose is on
 #define DBO_NOCREATE 0x0002 ///< prevent automatic creation of schema
@@ -66,7 +67,6 @@ public:
 	// mysql handle
 private:
 	MYSQL *mysql;
-	MYSQL *mysql_client; ///< connection handle
 public:
 	inline MYSQL *get_handle() { return mysql; };
 
