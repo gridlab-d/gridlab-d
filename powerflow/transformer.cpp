af9df139cb207cefdf4bb77a7b1f1b66aa303114
fish334 <andrew.fisher@pnnl.gov>
2017-04-26 09:17:29 -0700

    Revert "separated VLL and VLN for delta  or why respectively"
    This reverts commit e3c14974cddaf3bd4dffd56836fbe8a47997a30b.

--- powerflow/transformer.cpp
+++ powerflow/transformer.cpp
@@ -172,14 +172,10 @@ int transformer::init(OBJECT *parent)
 
 	link_object::init(parent);
 	OBJECT *obj = OBJECTHDR(this);
-	if ((config->connect_type == config->WYE_WYE) || (config->connect_type == config->DELTA_GWYE))
-		V_base = (config->V_secondary)* sqrt(3); // V_base is VLN so converting it to VLL. Hence the 3
-	else
-		V_base = config->V_secondary;
 
-
+	V_base = config->V_secondary;
 	voltage_ratio = nt = config->V_primary / config->V_secondary;
-	zt = (config->impedance * V_base * V_base) / (config->kVA_rating * 1000.0);
+	zt = (config->impedance * 3 * V_base * V_base) / (config->kVA_rating * 1000.0); // V_base is VLN so converting it to VLL. Hence the 3
 	zc =  complex(V_base * V_base,0) / (config->kVA_rating * 1000.0) * complex(config->shunt_impedance.Re(),0) * complex(0,config->shunt_impedance.Im()) / complex(config->shunt_impedance.Re(),config->shunt_impedance.Im());
 	//if (config->phaseA_kVA_rating )
 	//{
