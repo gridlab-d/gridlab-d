a26822b32309b4de25be70ab4484e92d44ada35b
afisher1 <andrew.fisher@pnnl.gov>
2017-06-01 08:55:36 -0700

    (release/RC4.0) removing experimental and unsupported modules for release.

--- GLDebug/runGLDebug.bat
+++
@@ -1,5 +0,0 @@
-rem echo off
-set GLCLASSPATH=GLDebug.jar
-set GLCLASSPATH=%GLCLASSPATH%;.\lib\jdom.jar
-
-java -Xmx200m -classpath %GLCLASSPATH% gov.pnnl.gridlab.gldebug.GLDebug
--- GLDebug/src/gov/pnnl/gridlab/gldebug/model/GLStatus.java
+++
@@ -1,8 +0,0 @@
-package gov.pnnl.gridlab.gldebug.model;
-
-/**
- * 
- */
-public enum GLStatus { 
-	NONE, RUNNING, STOPPED 
-}
