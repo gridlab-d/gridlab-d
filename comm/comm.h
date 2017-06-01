55bec2c4e66119c6ee00185716e4cd225335eddd
afisher1 <andrew.fisher@pnnl.gov>
2017-06-01 09:03:49 -0700

    (release/RC4.0) Removing experimental and unsupported modules for release 4.0

--- comm/comm.h
+++
@@ -1,9 +0,0 @@
-#ifndef _COMM_H
-#define _COMM_H
-
-#include "gridlabd.h"
-#include "module.h"
-
-#endif
-
-// EOF
--- python/gridspice/element/powerflow/powerflow_library.py
+++
@@ -1,9 +0,0 @@
-from .. import elementBase
-
-class powerflow_library(elementBase.elementBase):
-	def __init__(self):
-		pass
-
-
-
-
