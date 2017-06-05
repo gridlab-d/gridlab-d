c8b6b3b1364b125adc43ae74c0976af599c265f9
natet <nathan.tenney@pnnl.gov>
2017-06-05 01:46:53 -0700

    Added build_installer.py to the root build directory. Has the return code forwarded so that buildbot will pick it up.

---
+++ build_installer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+import time
+import subprocess
+
+installer_prefix = "gridlabd-nightly"
+if len(sys.argv) > 1:
+    installer_prefix = sys.argv[1]
+
+installer_script = "gridlabd.iss"
+if len(sys.argv) > 2:
+    installer_script = sys.argv[2]
+
+(y,m,d) = time.localtime()[:3]
+#y = str(y)[2:]
+#installer_name = installer_prefix + "_%02d%02d%s"%(m,d,y)
+
+installer_name = installer_prefix + "-%4d_%02d_%02d-nightly"%(y,m,d)
+
+#gridlabd-win32-yyyy_mm_dd-nightly.exe
+
+code = subprocess.call(["iscc.exe","/F"+installer_name ,installer_script],shell=True) 
+code = subprocess.call(["iscc.exe","/F"+installer_prefix+"-nightly" ,installer_script],shell=True) 
+
+exit(code)
