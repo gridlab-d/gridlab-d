f83cf7a07cadde9573f28ffa9339a26bd1ca772c
ftuffner <francis.tuffner@pnnl.gov>
2017-08-25 11:44:53 -0700

    (#730) General small fixes Fixed a typo in line.cpp Fixed a missing verbose for deltamode in generators/init Fixed the "casing" on voltdump to be consistent with GridLAB-D practices

--- powerflow/line.cpp
+++ powerflow/line.cpp
@@ -180,7 +180,7 @@ void line::load_matrix_based_configuration(complex Zabc_mat[3][3], complex Yabc_
 	{
 		if (use_line_cap == false)
 		{
-			gl_warning("Shunt capacitance of line:%s specified without setting powerflow::line_capacitance = TRUE. Shunt capacitance will be ignotred.",OBJECTHDR(this)->name);
+			gl_warning("Shunt capacitance of line:%s specified without setting powerflow::line_capacitance = TRUE. Shunt capacitance will be ignored.",OBJECTHDR(this)->name);
 
 			for (int i = 0; i < 3; i++) 
 			{
