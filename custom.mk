54848c92f81ec092be77f73eb773d4ffe4f61c38
David P. Chassin <dchassin@slac.stanford.edu>
2018-06-26 14:12:30 -0700

    Fixed custom.mk for default build and replaced gridlabd.in

--- custom.mk
+++ custom.mk
@@ -5,7 +5,7 @@
 #include matlab/Makefile.mk
 #include network/Makefile.mk
 #include plc/Makefile.mk
-include gismo/Makefile.mk
+#include gismo/Makefile.mk
 #include transactive/Makefile.mk
 #include powernet/Makefile.mk
-include blockchain/Makefile.mk
+#include blockchain/Makefile.mk
