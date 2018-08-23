311e3229d54052fe7516d5cfdde474df64cb47c3
David P. Chassin <dchassin@slac.stanford.edu>
2018-08-23 16:37:39 -0700

    Fixed bugs with the centos scripts

--- utilities/docker/centos-setup.sh
+++ utilities/docker/centos-setup.sh
@@ -8,9 +8,9 @@
 #   Password: ...
 #   ...
 #   [root@... /]# cd /tmp
-#   [root@... /]# curl https://raw.githubusercontent.com/dchassin/gridlab/master/utilties/centos-setup.sh -o setup.sh
-#   [root@... /]# chmod +x setup.sh
-#   [root@... /]# ./setup.sh
+#   [root@... /tmp]# curl https://raw.githubusercontent.com/dchassin/gridlabd/master/utilities/docker/centos-setup.sh -o setup.sh
+#   [root@... /tmp]# chmod +x setup.sh
+#   [root@... /tmp]# ./setup.sh
 #
 
 ### Docker commands to build gridlabd
@@ -22,33 +22,32 @@ yum groupinstall "Development Tools" -y
 yum install cmake -y 
 yum install https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm -y
 #yum install mysql56-server -y 
-MYSQL=mysql-connector-c-6.1.11-linux-glibc2.12-x86_64
-curl https://raw.githubusercontent.com/dchassin/gridlabd/master/third_party/${MYSQL}.tar.zip -o ${MYSQL}.tar.zip
-unzip -y ${MYSQL}.tar
-tar xf ${MYSQL}.tar
-rm -f ${MYSQL}.tar
-cp -u ${MYSQL}/bin/* /usr/local/bin
-cp -Ru ${MYSQL}/include/* /usr/local/include
-cp -Ru ${MYSQL}/lib/* /usr/local/lib
-rm -rf ${MYSQL}*
 yum install mysql-libs -y
-
-# install armadillo
-cd /usr/local/src
-curl https://raw.githubusercontent.com/dchassin/gridlabd/master/third_party/armadillo-7.800.1.tar.xz -o armadillo-7.800.1.tar.xz
-tar xf armadillo-7.800.1.tar.xz
-rm -f armadillo-7.800.1.tar.xz
-cd armadillo-7.800.1
-cmake .
-make install
 
 cd /usr/local/src
 git clone https://github.com/dchassin/gridlabd gridlabd
 
 # install xercesc
-cd gridlabd/third_party
+cd /usr/local/src/gridlabd/third_party
 alias sudo=
 . install_xercesc
+
+# install mysql 
+cd /usr/local/src/gridlabd/third_party
+MYSQL=mysql-connector-c-6.1.11-linux-glibc2.12-x86_64
+gunzip ${MYSQL}.tar.gz
+tar xf ${MYSQL}.tar
+cp -u ${MYSQL}/bin/* /usr/local/bin
+cp -Ru ${MYSQL}/include/* /usr/local/include
+cp -Ru ${MYSQL}/lib/* /usr/local/lib
+
+# install armadillo
+cd /usr/local/src/gridlabd/third_party
+gunzip armadillo-7.800.1.tar.gz
+tar xf armadillo-7.800.1.tar
+cd armadillo-7.800.1
+cmake .
+make install
 
 # install gridlabd
 cd /usr/local/src/gridlabd
