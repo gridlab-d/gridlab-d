1b0d4bbc1dd73984ee663131c58f7fd3f881fe02
David P. Chassin <dchassin@slac.stanford.edu>
2018-10-18 12:12:02 -0700

    Added support for mysqld container

--- utilities/docker/centos-mysql-apache-gridlabd/mysql-setup.sql
+++ utilities/docker/centos-mysql-apache-gridlabd/mysql-setup.sql
@@ -1,6 +1,16 @@
-CREATE USER 'gridlabd'@'localhost' IDENTIFIED BY 'gridlabd';
-GRANT ALL PRIVILEGES ON *.* TO 'gridlabd'@'localhost' WITH GRANT OPTION;
+CREATE USER gridlabd_a@localhost IDENTIFIED BY 'gridlabd';
+GRANT ALL PRIVILEGES ON *.* TO gridlabd_a@localhost WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-CREATE USER 'gridlabd_ro'@'%' IDENTIFIED BY 'gridlabd';
-GRANT SELECT ON *.* TO 'gridlabd_ro'@'%';
+
+CREATE USER gridlabd@localhost IDENTIFIED BY 'gridlabd';
+GRANT ALTER ON *.* TO gridlabd@localhost WITH GRANT OPTION;
+GRANT CREATE ON *.* TO gridlabd@localhost WITH GRANT OPTION;
+GRANT DROP ON *.* TO gridlabd@localhost WITH GRANT OPTION;
+GRANT SELECT ON *.* TO gridlabd@localhost WITH GRANT OPTION;
+GRANT UPDATE ON *.* TO gridlabd@localhost WITH GRANT OPTION;
+GRANT INSERT ON *.* TO gridlabd@localhost WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+CREATE USER gridlabd_ro@'%' IDENTIFIED BY 'gridlabd';
+GRANT SELECT ON *.* TO gridlabd_ro@'%';
 FLUSH PRIVILEGES;
