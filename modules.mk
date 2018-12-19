746d9a290d1e29d34e7e0c14b68b5bfb6b40f869
David P. Chassin <dchassin@slac.stanford.edu>
2018-12-19 10:36:10 -0800

    Moved list of gridlabd modules to a separate modules.mk file

---
+++ modules.mk
@@ -0,0 +1,15 @@
+# module.mk -- list of GridLAB-D modules that are consider part of this build
+include assert/Makefile.mk
+include climate/Makefile.mk
+include commercial/Makefile.mk
+include connection/Makefile.mk
+include generators/Makefile.mk
+include market/Makefile.mk
+include mysql/Makefile.mk
+include optimize/Makefile.mk
+include powerflow/Makefile.mk
+include reliability/Makefile.mk
+include residential/Makefile.mk
+include tape_file/Makefile.mk
+include tape/Makefile.mk
+include tape_plot/Makefile.mk
