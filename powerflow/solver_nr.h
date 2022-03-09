8af90b9ec543393d9c14c8ef725dadf9cd6bc17a
ftuffner <francis.tuffner@pnnl.gov>
2022-03-08 23:05:02 -0800

    (#1319) Fix for dynamic generators, including initializaton code for powerflow

--- powerflow/solver_nr.h
+++ powerflow/solver_nr.h
@@ -171,6 +171,7 @@ typedef struct {
 
 int64 solver_nr(unsigned int bus_count, BUSDATA *bus, unsigned int branch_count, BRANCHDATA *branch, NR_SOLVER_STRUCT *powerflow_values, NRSOLVERMODE powerflow_type , NR_MESHFAULT_IMPEDANCE *mesh_imped_vals, bool *bad_computations);
 void compute_load_values(unsigned int bus_count, BUSDATA *bus, NR_SOLVER_STRUCT *powerflow_values, bool jacobian_pass, int island_number);
+void NR_admittance_update(unsigned int bus_count, BUSDATA *bus, unsigned int branch_count, BRANCHDATA *branch, NR_SOLVER_STRUCT *powerflow_values, NRSOLVERMODE powerflow_type);
 
 //Newton-Raphson solver array handlers
 STATUS NR_array_structure_free(NR_SOLVER_STRUCT *struct_of_interest,int number_of_islands);		/* Handles freeing NR_SOLVER_STRUCT arrays */
