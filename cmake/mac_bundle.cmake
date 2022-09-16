cfdf8c2824cd3d5654f7faf03859bc82852f0e19
Mark Eberlein <mark.eberlein@pnnl.gov>
2022-09-15 18:49:10 -0600

    Fix MacOS build and attempt cross-compile

--- cmake/mac_bundle.cmake
+++
@@ -1,5 +0,0 @@
-include(BundleUtilities)
-
-set(BUNDLE_LIBS ${GLD_LIBS} ${GLD_DEPS})
-fixup_bundle(${CMAKE_INSTALL_PREFIX}/gridlabd.app ${BUNDLE_LIBS} "")
-verify_app(${CMAKE_INSTALL_PREFIX}/gridlabd.app)
