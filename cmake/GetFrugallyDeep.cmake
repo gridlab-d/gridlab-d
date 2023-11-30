c808148113efcca2d56eee1ac1ac1d3b4d9d6670
Mark Eberlein <mark.eberlein@pnnl.gov>
2023-11-30 10:28:53 -0700

    Fix some dependency target issues

--- cmake/GetFrugallyDeep.cmake
+++ cmake/GetFrugallyDeep.cmake
@@ -1,44 +1,35 @@
-# Version using HTTPS:
-# FetchContent_Declare(
-#   frugally-deep
-#   GIT_REPOSITORY https://github.com/Dobiasd/frugally-deep.git
-#   GIT_TAG        v0.15.26-p0
-# )
-# FetchContent_Declare(
-#   FunctionalPlus
-#   GIT_REPOSITORY https://github.com/Dobiasd/FunctionalPlus.git
-#   GIT_TAG        v0.2.20-p0
-# )
-# FetchContent_Declare(
-#   Eigen3
-#   GIT_REPOSITORY https://github.com/eigen-mirror/eigen.git
-#   GIT_TAG        3.4.0
-# )
-# FetchContent_Declare(
-#   nlohmann_json
-#   GIT_REPOSITORY https://github.com/nlohmann/json.git
-#   GIT_TAG        v3.11.2
-# )
+FetchContent_Declare(
+        frugally-deep
+        DOWNLOAD_EXTRACT_TIMESTAMP
+        URL https://github.com/Dobiasd/frugally-deep/archive/refs/tags/v0.15.26-p0.tar.gz
+#        GIT_REPOSITORY git@github.com:Dobiasd/frugally-deep.git
+#        GIT_TAG v0.15.26-p0
+        FIND_PACKAGE_ARGS CONFIG
 
-FetchContent_Declare(
-  frugally-deep
-  GIT_REPOSITORY git@github.com:Dobiasd/frugally-deep.git
-  GIT_TAG        v0.15.26-p0
 )
 FetchContent_Declare(
-  FunctionalPlus
-  GIT_REPOSITORY git@github.com:Dobiasd/FunctionalPlus.git
-  GIT_TAG        v0.2.20-p0
+        FunctionalPlus
+        DOWNLOAD_EXTRACT_TIMESTAMP
+        URL https://github.com/Dobiasd/FunctionalPlus/archive/refs/tags/v0.2.20-p0.tar.gz
+#        GIT_REPOSITORY git@github.com:Dobiasd/FunctionalPlus.git
+#        GIT_TAG v0.2.20-p0
+        FIND_PACKAGE_ARGS CONFIG
 )
 FetchContent_Declare(
-  Eigen3
-  GIT_REPOSITORY git@github.com:eigen-mirror/eigen.git
-  GIT_TAG        3.4.0
+        Eigen3
+        DOWNLOAD_EXTRACT_TIMESTAMP
+        URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
+#        GIT_REPOSITORY git@github.com:eigen-mirror/eigen.git
+#        GIT_TAG 3.4.0
+        FIND_PACKAGE_ARGS CONFIG NO_MODULE
 )
-FetchContent_Declare(
-  nlohmann_json
-  GIT_REPOSITORY git@github.com:nlohmann/json.git
-  GIT_TAG        v3.11.2
+FetchContent_Declare(json
+        DOWNLOAD_EXTRACT_TIMESTAMP
+        URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
+        FIND_PACKAGE_ARGS CONFIG
 )
 
-FetchContent_MakeAvailable(frugally-deep FunctionalPlus Eigen3 nlohmann_json)
+# This bypasses nlohmann_json's configutation where it will only install its targets if it thinks it is the top-level project
+# Frugally-deep is unable to build if these targets are not exported.
+SET(JSON_Install ON)
+FetchContent_MakeAvailable(json Eigen3 FunctionalPlus frugally-deep)
