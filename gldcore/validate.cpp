c29cb0b2065f3d23acc3b1cbaafe967d3fc05e88
Arun Veeramany <Arun.Veeramany@pnnl.gov>
2025-11-03 09:44:01 -0800

    n_procs set to processor_count() by default; commented out n_procs=fmin(...); raise separate ticket for this

--- gldcore/validate.cpp
+++ gldcore/validate.cpp
@@ -1025,7 +1025,7 @@ int validate(int argc, char *argv[])
 	// int n_procs = global_threadcount;
 	if (n_procs == 0)
 		n_procs = processor_count();
-	n_procs = fmin(final.get_tested(), (unsigned)n_procs);
+	// n_procs = fmin(final.get_tested(), (unsigned)n_procs);
 
 	// pthread_t *pid = new pthread_t[n_procs];
 	// output_debug("starting validation with cmdargs '%s' using %d threads", validate_cmdargs, n_procs);
