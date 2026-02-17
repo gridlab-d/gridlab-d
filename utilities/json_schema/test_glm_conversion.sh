bfb3f7c3b9c68f67ce661bb33a050bfd3862bc73
d3j331 <mitch.pelton@pnnl.gov>
2026-02-17 11:25:03 -0800

    Remove output files

--- utilities/json_schema/test_glm_conversion.sh
+++ utilities/json_schema/test_glm_conversion.sh
@@ -17,6 +17,10 @@ echo -e "\n== Test TE_Challenge =="
 python glm_to_json.py -d $indir -o $outdir TE_Challenge
 echo -e "\n== Test TEController =="
 python glm_to_json.py -d $indir -o $outdir TEController
+echo -e "\n== Test test_solargains =="
+python glm_to_json.py -d $indir -o $outdir test_solargains
+echo -e "\n== Test test_triplex_meter_parent_accum_NR_FPI =="
+python glm_to_json.py -d $indir -o $outdir test_triplex_meter_parent_accum_NR_FPI
 echo -e "\n== Test water_and_setpoint_schedule_v3 =="
 python glm_to_json.py -d $indir -o $outdir water_and_setpoint_schedule_v3
 
@@ -32,9 +36,15 @@ python validate_json.py solar.json
 echo -e "\n== Validate Te_Challange_test =="
 python validate_json.py Te_Challange_test.json
 echo -e "\n== Validate TE_Challenge =="
+python validate_json.py -TE_Challenge.json
+echo -e "\n== Validate TE_Challenge =="
 python validate_json.py TE_Challenge.json
 echo -e "\n== Validate TEController =="
 python validate_json.py TEController.json
+echo -e "\n== Validate test_solargains =="
+python validate_json.py test_solargains.json
+echo -e "\n== Validate test_triplex_meter_parent_accum_NR_FPI =="
+python validate_json.py test_triplex_meter_parent_accum_NR_FPI.json
 echo -e "\n== Validate water_and_setpoint_schedule_v3 =="
 python validate_json.py water_and_setpoint_schedule_v3.json
 
