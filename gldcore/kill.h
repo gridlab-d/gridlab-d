e5ebaaf0c445a5083416b1a1017d55f55bdd8e1a
Arun Veeramany <arun.veeramany@pnnl.gov>
2026-02-28 06:25:10 -0800

    pid_t for msvc

--- gldcore/kill.h
+++ gldcore/kill.h
@@ -4,13 +4,39 @@
 #ifndef _KILL_H
 #define _KILL_H
 
-#ifdef _WIN32
-#ifdef __MINGW32__
+#if defined(_WIN32)
+#include <process.h> // _getpid
+// MSVC doesn’t define pid_t; MinGW does in <sys/types.h>.
+#ifndef __MINGW32__
+typedef int pid_t;
+#else
 #include <sys/types.h>
 #endif
-void kill_starthandler(void);
-void kill_stophandler(void);
-int kill(pid_t pid, int sig);
+#else
+#include <sys/types.h> // pid_t on POSIX
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	void kill_starthandler(void);
+	void kill_stophandler(void);
+	int kill(pid_t pid, int sig); // Provide a Windows implementation separately
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#if defined(_WIN32)
+static inline void kill_starthandler(void) { /* no-op on Windows */ }
+static inline void kill_stophandler(void) { /* no-op on Windows */ }
+static inline int kill(pid_t /*pid*/, int /*sig*/)
+{
+	/* return -1 to indicate "not supported" on Windows by default */
+	return -1;
+}
 #endif
 
 #endif
