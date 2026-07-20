0af7db72b277113ceb9737b1a55b3ecfc0841976
d3j331 <mitch.pelton@pnnl.gov>
2026-07-20 13:24:05 -0700

    MS VS version

--- powerflow/recloser.cpp
+++ powerflow/recloser.cpp
@@ -255,9 +255,9 @@ extern "C" MODULE_API int isa_recloser(OBJECT *obj, char *classname) {
 extern "C" MODULE_API int isa_recloser(OBJECT *obj, ...) {
   va_list args;
   va_start(args, obj);
-  char *classsname = va_arg(args, char *);
+  char *classname = va_arg(args, char *);
   va_end(args);
-  return isa_recloser_impl(obj, classsname);
+  return isa_recloser_impl(obj, classname);
 }
 #endif
 
